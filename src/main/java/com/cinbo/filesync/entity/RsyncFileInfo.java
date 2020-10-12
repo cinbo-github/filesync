c03c2cd76b62b85e58c6e527741718736e5950fa
cinbo <cinbo@163.com>
2020-10-12 17:31:18 +0800

    增加sqlite

--- src/main/java/com/cinbo/filesync/entity/RsyncFileInfo.java
+++ src/main/java/com/cinbo/filesync/entity/RsyncFileInfo.java
@@ -2,8 +2,17 @@ package com.cinbo.filesync.entity;
 
 import java.io.Serializable;
 
+/**
+ * 同步两端的文件交互信息
+ */
 public class RsyncFileInfo implements Serializable {
 
+    /**
+     * 文件同步模式,同步模式决定了以下参数哪些是可用，哪些是不可用。
+     */
+    private RsyncMode rsyncMode;
+
+    //===========================================Rsync模式===================================//
     /**
      * 每次读取文件的block大小
      */
@@ -13,6 +22,8 @@ public class RsyncFileInfo implements Serializable {
      * 滑动检验的block大小，默认值是1024;
      */
     private int rsyncRollingCheckBlockSize = 1024;
+
+
 
     /**
      * 同步文件，目标文件的文件路径包含文件名
